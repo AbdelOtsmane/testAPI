75c242ef6f05f056f4e04d049bdcf0d111801602
Abdel <otsmane.abdel@gmail.com>
2024-04-12 02:55:27 +0200

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors'); // Importer le module cors
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Utiliser le middleware cors
+app.use(cors());
+
+// Route principale
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
